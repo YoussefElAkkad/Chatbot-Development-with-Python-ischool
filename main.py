5ad3416dbcbf5560d937ab909a0906a8636dda1d
Youssef Tamer Ismail Moustafa ElAkkad <161077620+YoussefElAkkad@users.noreply.github.com>
2024-08-02 08:04:22 +0000

    Session1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+name= "Youssef"
+print("Welcome to Replit, "+ name+"\n")
+print('''
+congratulations,
+Welcome to Replit, I am happy to meet you %s.'''%name)
+
+print('''Hello
+world %s'''%name)
