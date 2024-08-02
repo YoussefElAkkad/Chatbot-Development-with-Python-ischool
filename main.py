576aa859d5dab38aaf70a8c03cb5d312d7c36c95
Youssef Tamer Ismail Moustafa ElAkkad <161077620+YoussefElAkkad@users.noreply.github.com>
2024-08-02 09:34:33 +0000

    .

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-name= "Youssef"
-print("Welcome to Replit, "+ name+"\n")
-print('''
-congratulations,
-Welcome to Replit, I am happy to meet you %s.'''%name)
+#Calculation Example
+#print(2+2)
+#print(20+10*30)
+#print(10*20+30)
 
-print('''Hello
-world %s'''%name)
+#x=int(input("Enter a number: "))
+#print(x)
+
+
+#first_day_coins = 20
+#coins_every_day = 10
+#print( first_day_coins+ #coins_every_day * 30 )
+
+
+
+first_day_coins = 20
+coins_every_day = int(input("What is the number of coins you get every day? "))
+print( first_day_coins+ coins_every_day * 30 )
