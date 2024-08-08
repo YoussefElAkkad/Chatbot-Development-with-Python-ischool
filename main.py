16ed268e031637e201ef204fd54230f34ab8c039
Youssef Tamer Ismail Moustafa ElAkkad <161077620+YoussefElAkkad@users.noreply.github.com>
2024-08-08 20:47:53 +0000

    ..

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-name = input("Please enter the student name\n")
+#String Embedding
 
-email = "Dear %s, there will be an exam in 10/4\nthank you\n"
+#name="My name is %s"
+#print(name % "Youssef")
+#print(name[3])
 
-Mr = "M. Shlah"
 
-print( name[0].upper() + '\n' + email % name + Mr)
+
+#String Upper and Lower cases
+
+Mr = "mr. Salah"
+mr_first_letter_capital=Mr.capitalize()
+mr_capital=Mr.upper()
+mr_small=Mr.lower()
+
+print(mr_first_letter_capital)
+print(mr_capital)
+print(mr_small)
+
+
+
+#String Multiplication
+
+string="youssef "
+print(string*5)
+
+
+#String Substring
+new_string=string[1:len(string)]
+print(new_string)
+
+#String Concaenation
+print("Length of %s is %s"%(string,len(string)))
+print(string+new_string)
+
+#Final 
+
+#name = input("Please enter the student name\n")
+#email = "Dear %s, there will be an exam in 10/4\nthank you\n"
+#print( name[0].upper() + '\n' + email % name + Mr)
+
