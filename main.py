283a70fc27f8654752e715b5818999103c4bc08f
Youssef Tamer Ismail Moustafa ElAkkad <161077620+YoussefElAkkad@users.noreply.github.com>
2024-08-08 20:03:46 +0000

    .

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
+name = input("Please enter the student name\n")
 
-#first_day_coins = 20
-#coins_every_day = int(input("What is the number of coins you get every day? "))
-#print( first_day_coins+ #coins_every_day * 30 )
+email = "Dear %s, there will be an exam in 10/4\nthank you\n"
 
-joke="Do you know what object has four legsand can't move?\nIt's a chair"
-joke2='''Do you know what object has four legsand can't move? 
-It's a chair'''
-print(joke)
-print(joke2)
+Mr = "M. Shlah"
 
-silly_string = '''He said, "Aren't can't shouldn't wouldn't."'''
-print(silly_string)
-
-silly_string2 = '''He said, "Aren't can't shouldn't wouldn't." (\''')'''
-print(silly_string2)
+print( name[0].upper() + '\n' + email % name + Mr)
