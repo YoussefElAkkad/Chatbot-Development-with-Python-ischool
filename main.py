621aee2eb8d7d87572f821578c57dbd16fddc771
Youssef Tamer Ismail Moustafa ElAkkad <161077620+YoussefElAkkad@users.noreply.github.com>
2024-08-13 22:37:17 +0000

    .

--- main.py
+++ main.py
@@ -15,6 +15,31 @@
 # print(shopping_list)
 
 
-string = 'This is my string that I will convert to list'
-list = (string.split())
-print(list)
+# string = 'This is my string that I will convert to list'
+# list = (string.split()) # by white space 
+# print(list)
+
+
+
+
+
+
+# assignment1 = [
+#   ['Omar', 89,],
+#   ['Ali', 90],
+#   ['Emad', 94],
+#   ['Samy', 92]
+# ] #List of Lists(nested_Lists)
+
+# assignment1.append(['Amr', 90])
+
+# print(assignment1)
+# print(assignment1[0][1])
+# print("\nThe number of students in assignment 1 is:" ,len(assignment1))
+
+
+
+# x=[1,2,3,4,5]
+# y=[6,7,8,9,10]
+# print(x+y)
+# print(x*3)
