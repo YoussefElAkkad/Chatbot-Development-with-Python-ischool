536bec974da99324294c83fa967ac3fd45426b11
Youssef Tamer Ismail Moustafa ElAkkad <161077620+YoussefElAkkad@users.noreply.github.com>
2024-08-14 19:07:47 +0000

    .

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#cdcs
+# money = int(input("Enter the amount of money: "))
+# if (money >= 100 and money <= 500):
+#   print("The amount of money is within the first range.")
+# elif  (money >= 1000 and money <= 5000):
+#   print("The amount of money is within the second range.")
+# else:
+#     print("The amount of money is outside the range.")
