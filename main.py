d7058fa8d19b40e7e8d7f43afeab8ae03bf045c9
Youssef Tamer Ismail Moustafa ElAkkad <161077620+YoussefElAkkad@users.noreply.github.com>
2024-08-22 20:08:20 +0000

    .

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-# money = int(input("Enter the amount of money: "))
-# if (money >= 100 and money <= 500):
-#   print("The amount of money is within the first range.")
-# elif  (money >= 1000 and money <= 5000):
-#   print("The amount of money is within the second range.")
+# n=int(input("Enter a number: "))
+# sum=0
+# for i in range(1,n+1):
+#   sum+=i
+#   print(i)
+# print(sum)
+
+
+
+# n = int(input("Enter the number of rows: "))
+# for i in range(1, n + 1):
+#     for j in range(i):
+#         print("*", end="")
+#     print()
+
+# n = int(input("Enter a number: "))
+# if(n%2==0): #Even Number
+#   print("Even Number")
+#   j=0
 # else:
-#     print("The amount of money is outside the range.")
+#   print("Odd Number")
+#   j=1
+# for i in range(j, n + 1, 2):
+#  print(i)
