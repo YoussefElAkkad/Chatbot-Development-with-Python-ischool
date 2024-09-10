6c0078a22b7515b6fecc9c751a0187baed62b6bb
Youssef Tamer Ismail Moustafa ElAkkad <161077620+YoussefElAkkad@users.noreply.github.com>
2024-09-10 15:54:31 +0000

    .

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello")
+class Things:
+  pass
+
+class Animate(Things):
+  def walk(self):
+    print("I can walk")
+
+
+class Mammals(Animate):
+  pass
+
+class Giraffes(Mammals):
+  pass
+
+joe=Giraffes()
+joe.walk()
