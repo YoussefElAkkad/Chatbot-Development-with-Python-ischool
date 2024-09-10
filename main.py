fb80cbea438e5cba252ba5d3d7b4fd88c878b657
Youssef Tamer Ismail Moustafa ElAkkad <161077620+YoussefElAkkad@users.noreply.github.com>
2024-09-10 16:42:35 +0000

    .

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-class Things:
-  pass
+# class Things:
+#   pass
 
-class Animate(Things):
-  def walk(self):
-    print("I can walk")
+# class Animate(Things):
+#   def eat(self):
+#     print("I can eat")
+#   def walk(self):
+#     print("I can walk")
 
 
-class Mammals(Animate):
-  pass
+# class Mammals(Animate):
+#   pass
 
-class Giraffes(Mammals):
-  pass
+# class Giraffes(Mammals):
+#   def eat(self):
+#     super().eat()
+#     print("Eating leaves")
 
-joe=Giraffes()
-joe.walk()
+# class Lions(Mammals):
+#   def eat(self):
+#    super().eat()
+#    print("Eating Meat")
+
+
+
+# Mammal_one=Mammals()
+# Mammal_one.eat()
+# joe=Giraffes()
+# joe.walk()
+# joe.eat()
+# Simba=Lions()
+# Simba.eat()
+
+
+
+
+
+class WatermelonSlice:
+  def __init__(self,shape,color):
+    self.shape=shape
+    self.color=color
+  def area(self):
+    pass
+    
+class CircularSlice(WatermelonSlice):
+  def __init__(self, color, radius):
+    super().__init__("Circular", color)
+    self.radius = radius
+  def area(self):
+    return 3.14 * self.radius ** 2
+  
+class RectangularSlice(WatermelonSlice):
+  def __init__(self, color, length, width):
+    super().__init__("Rectangular", color)
+    self.length = length
+    self.width = width
+  def area(self):
+    return self.length * self.width
+
+circle_slice = CircularSlice("Red", 5)
+rectangle_slice = RectangularSlice("Blue", 3, 4)
+
+print("Circular Slice Area:", circle_slice.area())
+print("Rectangular Slice Area:", rectangle_slice.area())
+print(circle_slice.shape)
+print(rectangle_slice.shape)
+
