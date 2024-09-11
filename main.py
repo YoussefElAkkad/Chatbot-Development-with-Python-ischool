8cf88e8d5972b4a4d317e153ac2308f5d50ba4a3
Youssef Tamer Ismail Moustafa ElAkkad <161077620+YoussefElAkkad@users.noreply.github.com>
2024-09-11 09:17:00 +0000

    .

--- main.py
+++ main.py
@@ -1,67 +1,28 @@
-# class Things:
-#   pass
+class Dog:
+  def __init__(self,breed):
+    self.breed=breed
+  def make_sound(self):
+    return "Woof!"
 
-# class Animate(Things):
-#   def eat(self):
-#     print("I can eat")
-#   def walk(self):
-#     print("I can walk")
+class Cat:
+  def __init__(self,breed):
+    self.breed=breed
 
+  def make_sound(self):
+    return "Meow!"
 
-# class Mammals(Animate):
-#   pass
-
-# class Giraffes(Mammals):
-#   def eat(self):
-#     super().eat()
-#     print("Eating leaves")
-
-# class Lions(Mammals):
-#   def eat(self):
-#    super().eat()
-#    print("Eating Meat")
-
+class AnimalShelter:
+  def __init__(self):
+    self.animals=[]
+  def add_animal(self,animal):
+    self.animals.append(animal)
+  def make_sounds(self):
+    for animal in self.animals:
+      print(animal.make_sound())
 
 
-# Mammal_one=Mammals()
-# Mammal_one.eat()
-# joe=Giraffes()
-# joe.walk()
-# joe.eat()
-# Simba=Lions()
-# Simba.eat()
-
-
-
-
-
-class WatermelonSlice:
-  def __init__(self,shape,color):
-    self.shape=shape
-    self.color=color
-  def area(self):
-    pass
-    
-class CircularSlice(WatermelonSlice):
-  def __init__(self, color, radius):
-    super().__init__("Circular", color)
-    self.radius = radius
-  def area(self):
-    return 3.14 * self.radius ** 2
-  
-class RectangularSlice(WatermelonSlice):
-  def __init__(self, color, length, width):
-    super().__init__("Rectangular", color)
-    self.length = length
-    self.width = width
-  def area(self):
-    return self.length * self.width
-
-circle_slice = CircularSlice("Red", 5)
-rectangle_slice = RectangularSlice("Blue", 3, 4)
-
-print("Circular Slice Area:", circle_slice.area())
-print("Rectangular Slice Area:", rectangle_slice.area())
-print(circle_slice.shape)
-print(rectangle_slice.shape)
-
+shelter=AnimalShelter()
+shelter.add_animal(Dog("Golden Retriever"))
+shelter.add_animal(Cat("Persian"))
+shelter.add_animal(Dog("Poodle"))
+shelter.make_sounds()
