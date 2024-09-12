acbfeddbc4999be35d1fd40ba74ca572dadd4dfd
YoussefElAkkad <eng.yousseftamer2001@gmail.com>
2024-09-12 18:19:49 +0300

    .

--- main.py
+++ main.py
@@ -29,21 +29,52 @@
 
 
 
-class Car:
-    def __init__(self,brand,model):
-        self.brand=brand
-        self.model=model
-    def drive(self):
-        return f"Driving {self.brand} {self.model}"
+# class Car:
+#     def __init__(self,brand,model):
+#         self.brand=brand
+#         self.model=model
+#     def drive(self):
+#         return f"Driving {self.brand} {self.model}"
 
-class ElectricCar:
-    def __init__(self,brand,model):
-        self.brand=brand
-        self.model=model
-    def drive(self):
-        return f"Driving {self.brand} {self.model} silently"
+# class ElectricCar:
+#     def __init__(self,brand,model):
+#         self.brand=brand
+#         self.model=model
+#     def drive(self):
+#         return f"Driving {self.brand} {self.model} silently"
     
-car=Car("Toyota","Camry")
-ECar=ElectricCar("Tesla","Model S")
-print(car.drive())
-print(ECar.drive())
+# car=Car("Toyota","Camry")
+# ECar=ElectricCar("Tesla","Model S")
+# print(car.drive())
+# print(ECar.drive())
+
+
+
+
+
+class Employee:
+    def __init__(self,name,salary):
+      self.name=name
+      self.salary=salary
+
+    def get_info(self):
+       return f"Name: {self.name}, Salary: {self.salary}"
+    
+class Manager(Employee):
+   def __init__(self,name,salary,department):
+      super().__init__(name,salary)
+      self.department=department
+
+   def get_info(self):
+      return f"{super().get_info()}, Department: {self.department}"
+   
+
+class Director(Manager):
+   def __init__(self,name,salary,department,num_of_reports):
+      super().__init__(name,salary,department)
+      self.num_of_reports=num_of_reports
+   def get_info(self):
+      return f"{super().get_info()}, Reports: {self.num_of_reports}"
+   
+d=Director("Alice",100000,"Finance",5)
+print(d.get_info())
