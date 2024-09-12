bb2d8b659d11365408633af96ee52e39bdf58b8b
YoussefElAkkad <eng.yousseftamer2001@gmail.com>
2024-09-12 18:44:20 +0300

    .

--- main.py
+++ main.py
@@ -52,29 +52,61 @@
 
 
 
-class Employee:
-    def __init__(self,name,salary):
-      self.name=name
-      self.salary=salary
+# class Employee:
+#     def __init__(self,name,salary):
+#       self.name=name
+#       self.salary=salary
 
-    def get_info(self):
-       return f"Name: {self.name}, Salary: {self.salary}"
+#     def get_info(self):
+#        return f"Name: {self.name}, Salary: {self.salary}"
     
-class Manager(Employee):
-   def __init__(self,name,salary,department):
-      super().__init__(name,salary)
-      self.department=department
+# class Manager(Employee):
+#    def __init__(self,name,salary,department):
+#       super().__init__(name,salary)
+#       self.department=department
 
-   def get_info(self):
-      return f"{super().get_info()}, Department: {self.department}"
+#    def get_info(self):
+#       return f"{super().get_info()}, Department: {self.department}"
    
 
-class Director(Manager):
-   def __init__(self,name,salary,department,num_of_reports):
-      super().__init__(name,salary,department)
-      self.num_of_reports=num_of_reports
-   def get_info(self):
-      return f"{super().get_info()}, Reports: {self.num_of_reports}"
+# class Director(Manager):
+#    def __init__(self,name,salary,department,num_of_reports):
+#       super().__init__(name,salary,department)
+#       self.num_of_reports=num_of_reports
+#    def get_info(self):
+#       return f"{super().get_info()}, Reports: {self.num_of_reports}"
    
-d=Director("Alice",100000,"Finance",5)
-print(d.get_info())
+# d=Director("Alice",100000,"Finance",5)
+# print(d.get_info())
+
+
+
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+    
+    def add_book(self, title, author):
+        book = {'title': title, 'author': author, 'available': True}
+        self.books.append(book)
+    
+    def remove_book(self, title):
+        for book in self.books:
+            if book['title'] == title:
+                self.books.remove(book)
+                break
+    
+    def display_available_books(self):
+        for book in self.books:
+            if book['available']:
+                print("Title:", book['title'])
+                print("Author:", book['author'])
+                print()
+
+library=Library()
+library.add_book("The Great Gatspy","F. Scott Fitzgerald")
+library.add_book("To Kill a Mockingbird","Harper Lee")
+library.display_available_books()
+library.remove_book("To Kill a Mockingbird")
+library.display_available_books()
