deb31ad8fbca888f2b1e3ad881f1975cc270c56c
YoussefElAkkad <eng.yousseftamer2001@gmail.com>
2024-09-13 12:33:54 +0300

    .

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print("Hello World")
+# if 5+5==10:
+#     print("What's the best way to speak to a monster")
+#     print("From as far away as possible!")
+
+# if '5'+'5'==10:
+#     print("yes")
+# else:
+#     print("NO")
+
+
+# print('5'+'5') #string concatenation
+
+
+# age='10'
+# print(age+age)
+# converted_age=int(age)
+# print(converted_age+converted_age)
+
+
+
+# money = int(input("What is your amount of money?"))
+
+# if money >=2000:
+#   print("I am rich.")
+# else:
+#   print(" I am not rich but I might be later .. :D.")
+
+
+
+# money = int(input("Enter the amount of money: "))
+# if (money >= 100 and money <= 500) or (money >= 1000 and money <= 5000):
+#     print("The amount of money is within the range.")
+# else:
+#     print("The amount of money is outside the range.")
+
+
+
+# ninjas = int(input("Enter the number of ninjas: "))
+# if ninjas < 10:
+#     print("I can fight those ninjas!")
+# elif ninjas < 30:
+#     print("It’ll be a struggle, but I can take ’em")
+# elif ninjas < 50:
+#     print("That’s too many")
