0f6d9f7bdf92f8415898b9ff7d85d6a0afd49a8b
YoussefElAkkad <eng.yousseftamer2001@gmail.com>
2024-09-16 19:04:01 +0300

    .

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-import turtle
-t=turtle.Turtle()
+# import turtle
+# t=turtle.Turtle()
+
 # for i in range(0,4):
 #     t.forward(50)
 #     t.left(90)
+
 
 # for i in range(0,8):
 #     t.forward(100)
 #     t.left(225)
 
+
+
 # for i in range(0,19):
 #     t.forward(100)
 #     t.left(95)
+
+
 
 # for i in range(0,18):
 #     t.forward(100)
@@ -18,5 +24,30 @@ t=turtle.Turtle()
 #         t.left(175)
 #     else:
 #         t.left(225)
-t.circle(50)
-turtle.done()
+
+
+
+# t.color(0.9,0.75,0.2)
+# t.begin_fill()
+# t.circle(50)
+# t.end_fill()
+
+# def mySquare(size):
+#     for x in range(0,4):
+#         t.forward(size)
+#         t.left(90)
+        
+# mySquare(25)
+# mySquare(50)
+# mySquare(75)
+# mySquare(100)
+
+# def draw_octagon():
+
+#     for x in range(8): 
+#         t.forward(100) 
+#         t.right(45)
+
+# draw_octagon()
+
+# turtle.done()
