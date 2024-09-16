2587a8999e70ad754121fb191a1a8c2cc8504634
YoussefElAkkad <eng.yousseftamer2001@gmail.com>
2024-09-16 18:47:38 +0300

    .

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import turtle
 t=turtle.Turtle()
-for i in range(0,4):
-    t.forward(50)
-    t.left(90)
+# for i in range(0,4):
+#     t.forward(50)
+#     t.left(90)
+
+# for i in range(0,8):
+#     t.forward(100)
+#     t.left(225)
+
+# for i in range(0,19):
+#     t.forward(100)
+#     t.left(95)
+
+# for i in range(0,18):
+#     t.forward(100)
+#     if i % 2==0:
+#         t.left(175)
+#     else:
+#         t.left(225)
+t.circle(50)
 turtle.done()
