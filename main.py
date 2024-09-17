ad899a30877b714a285dc28341a954fb2d21a8b0
Youssef Tamer Ismail Moustafa ElAkkad <eng.yousseftamer2001@gmail.com>
2024-09-17 19:15:57 +0300

    .

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello_World")
+# joke1=''' Do you know what object has four legs and can't move?
+# It's a chair'''
+# joke2=''' Do you know what............?
+# It's ............!'''
+# print(joke1)
+# print(joke2)
+
+
+# x=int(input("Please enter your Age:"))
+# print(x*2)
+
+# second_string="%s is the biggest city in the UK and the most expensive  to live"
+# print(second_string %"London")
+# third_string="I love %s because it is my country"
+# print(third_string %"Canada")
+
+# name=input("Please enter the student name \n")
+# email="Dear %s, there will be an exam in 10/4 \nThank you \n"
+# teacher="Mr. Salah"
+# print(email %name+ teacher)
+
+string1="hello"
+print(string1.upper())
+string2="YOUSSEF"
+print(string2.lower())
