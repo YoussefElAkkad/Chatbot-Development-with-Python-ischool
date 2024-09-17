8104a933fea7f54609da3e9c31b103e3bf59e8c7
YoussefElAkkad <eng.yousseftamer2001@gmail.com>
2024-09-17 14:26:41 +0300

    .

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello_World")
+from tkinter import *
+
+tk=Tk()
+#named parameters where the order of the parameters does not matter now 
+canvas=Canvas(tk,width=500,height=500) 
+canvas.pack()
+# canvas.create_line(0,0,500,500)
+# canvas.create_rectangle(10,10,100,100)
+#canvas.create_polygon(10,10,100,10,100,110,fill="",outline='black')
+my_image=PhotoImage(file='/media/youssef/Data1/eng.yousseftamer/ischool/Grade5_Semester2/B.jpg')
+canvas.create_image(0,0,anchor="NW",image=my_image)
+tk.mainloop()
