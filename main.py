7a606bdd90c6424d370d1002fae3ce161af3dc59
YoussefElAkkad <eng.yousseftamer2001@gmail.com>
2024-09-18 18:49:41 +0300

    .

--- main.py
+++ main.py
@@ -1,46 +1,25 @@
-# from tkinter import *
-# import time
-# tk=Tk()
-#named parameters where the order of the parameters does not matter now 
-# canvas=Canvas(tk,width=500,height=500) 
-# canvas.pack()
-# canvas.create_line(0,0,500,500)
-# canvas.create_rectangle(10,10,100,100)
-# canvas.create_polygon(10,10,100,10,100,110,fill="",outline='black')
-# my_image=PhotoImage(file='photo.png')
-# canvas.create_image(0,0,anchor="nw",image=my_image)
-# for x in range(0,60):
-#     canvas.move(2,5,0)
-#     tk.update()
-#     time.sleep(0.05)
+a=[]
+for x in range(1,11):
+    a.append(x*x)
+print("List a: %s"%a)
 
+b=[x**2 for x in range(1,11)] #list comprehension
+print("List b: %s"%b)
 
-# def moveTriangle(event):
-#     canvas.move(1,5,0)
-# canvas.bind_all("<KeyPress-Return>",moveTriangle)
-# tk.mainloop()
+#normal division
+result=5/3
+print(result)
+#integer division(floor division)
+result=5//3
+print(result)
 
+#f-strings(formatted StringLiterals )
+name="Alice"
+print(f"Hello, {name}!")
+print(f"List a: {a}")
 
+def double(x):
+    return x*2
 
-import time
-from tkinter import *
-tk = Tk()
-canvas = Canvas(tk, width=400, height=300)
-canvas.pack()
-canvas.create_polygon(10, 10, 10, 60, 50, 35)
-for x in range(0, 60):
-    canvas.move(1, 5, 0)
-    tk.update()
-    time.sleep(0.05)
-for y in range(0, 40):
-    canvas.move(1, 0, 5)
-    tk.update()
-    time.sleep(0.05)
-for x in range(0, 60):
-    canvas.move(1, -5, 0)
-    tk.update()
-    time.sleep(0.05)
-for y in range(0, 40):
-    canvas.move(1, 0, -5)
-    tk.update()
-    time.sleep(0.05)
+number=7
+print(f"Double of {number} is {double(number)}.")
